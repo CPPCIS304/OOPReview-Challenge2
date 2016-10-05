a7274add23dd6a554d3721d64d0d28271e2c36b0
Stephen Briles <stephenbriles@gmail.com>
2016-10-05 14:21:06 -0700

    LoanPayment class framework

--- FinancialCalculations/src/finance/LoanPayment.java
+++ FinancialCalculations/src/finance/LoanPayment.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package finance;
+
+import finance.Investment;
+import finance.enums.CompoundingOption;
 
 /**
  *
- * @author skbriles
+ * @author 127.0.0.1 Team
  */
+
 public class LoanPayment {
+        double purchaseAmount;
+        double downPayment;
+        double interestRate;
+        CompoundingOption compounding;
+        double loanDuration;
+        
+    /**
+    * The class constructor creates a LoanPayment object configured to calculate a loan payment. 
+    * Two constructors shall be provided - one without parameters and one with parameters. 
+    * When parameters are used, the loan parameters are provided to the object through the constructor.
+    */
+        
+    public LoanPayment() {
+        double purchaseAmount = 0;
+        double downPayment = 0;
+        double interestRate = 0;
+        CompoundingOption compounding;
+        double loanDuration = 0;
+        
+    }
     
+    public LoanPayment (double purchaseAmount, double downPayment, double interestRate, CompoundingOption compounding, double loanDuration) {
+        this.purchaseAmount = purchaseAmount;
+        this.downPayment = downPayment;
+        this.interestRate = interestRate;
+        
+        this.loanDuration = loanDuration;
+    }
+    
+    public void setPurchaseAmount() {
+     
+    }
+    
+    public void setDownPayment() {
+        
+    }
+    public double getPurchaseAmount() {
+        return 0;
+    }
+    
+    public double getDownPayment() {
+        return 0;
+    }
+    
+    public String getValue() {
+        return "null";
+    }
+    
+    public String print() {
+        return "null";
+    }
 }
